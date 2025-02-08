483b92372633ef47ca75485d12dbb762695f82d9
Elmer Cruz <elmer06.cruz@gmail.com>
2025-02-08 10:57:41 -0600

    Guardado de todo el proyecto

---
+++ src/components/Header.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+
+export const Header = ({
+    allProducts,
+    setAllProducts,
+    total,
+    setTotal,
+    countProducts,
+    setCountProducts
+}) => {
+    const [active, setActive] = React.useState(false);
+
+    const onDeleteProduct = product => {
+        const results = allProducts.filter(item => item.id !== product.id);
+        setTotal(total - (product.price * product.quantity));
+        setCountProducts(countProducts - product.quantity);
+        setAllProducts(results);
+    }
+
+    const onCleanCart = () => {
+        setAllProducts([]);
+        setTotal(0);
+        setCountProducts(0);
+    }
+
+    return (
+        <header>
+            <h1>Tienda de Libros</h1>
+            <div className="container-icon">
+                <div className="container-cart-icon" onClick={() => setActive(!active)}>
+                    <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMjgiIGhlaWdodD0iMTI4IiB2aWV3Qm94PSIwIDAgMjQgMjQiPjxwYXRoIGZpbGw9IiMwMDAwMDAiIGQ9Ik03IDIycS0uODI1IDAtMS40MTItLjU4N1Q1IDIwdC41ODgtMS40MTJUNyAxOHQxLjQxMy41ODhUOSAyMHQtLjU4NyAxLjQxM1Q3IDIybTEwIDBxLS44MjUgMC0xLjQxMi0uNTg3VDE1IDIwdC41ODgtMS40MTJUMTcgMTh0MS40MTMuNTg4VDE5IDIwdC0uNTg3IDEuNDEzVDE3IDIyTTUuMiA0aDE0Ljc1cS41NzUgMCAuODc1LjUxM3QuMDI1IDEuMDM3bC0zLjU1IDYuNHEtLjI3NS41LS43MzcuNzc1VDE1LjU1IDEzSDguMUw3IDE1aDExcS40MjUgMCAuNzEzLjI4OFQxOSAxNnQtLjI4OC43MTNUMTggMTdIN3EtMS4xMjUgMC0xLjctLjk4N3QtLjA1LTEuOTYzTDYuNiAxMS42TDMgNEgycS0uNDI1IDAtLjcxMi0uMjg4VDEgM3QuMjg4LS43MTJUMiAyaDEuNjI1cS4yNzUgMCAuNTI1LjE1dC4zNzUuNDI1eiIvPjwvc3ZnPg==" alt="carrito" className='icon-cart' />
+                    <div className="count-products">
+                        <span id="contador-productos">{countProducts}</span>
+                    </div>
+                </div>
+                <div className={`container-cart-products ${active ? '' : 'hidden-cart'}`}>
+                {allProducts.length ? (
+                        <>
+                        <div className="row-product">
+                            {allProducts.map(product => (
+                                <div className="cart-product" key={product.id}>
+                                    <div className="info-cart-product">
+                                        <span className="cantidad-producto-carrito">{product.quantity}</span>
+                                        <p className="titulo-producto-carrito">{product.title}</p>
+                                        <span className="precio-producto-carrito">${product.price}</span>
+                                    </div>
+                                    <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMjgiIGhlaWdodD0iMTI4IiB2aWV3Qm94PSIwIDAgMjQgMjQiPjxwYXRoIGZpbGw9IiMwMDAwMDAiIGQ9Ik0xOSAzSDVhMiAyIDAgMCAwLTIgMnYxNGEyIDIgMCAwIDAgMiAyaDE0YTIgMiAwIDAgMCAyLTJWNWEyIDIgMCAwIDAtMi0ybS0zLjQgMTRMMTIgMTMuNEw4LjQgMTdMNyAxNS42bDMuNi0zLjZMNyA4LjRMOC40IDdsMy42IDMuNkwxNS42IDdMMTcgOC40TDEzLjQgMTJsMy42IDMuNnoiLz48L3N2Zz4=" alt="cerrar" className='icon-close' onClick={() => onDeleteProduct(product)}/>
+                                </div>
+                            ))}
+                            <div className="cart-total">
+                                <h3>Total:</h3>
+                                <span className="total-pagar">${total}</span>
+                            </div>
+                            <button className="btn-clean-cart" onClick={onCleanCart}>Limpiar carrito</button>
+                        </div>
+                        </>
+                    ) : (
+                        <p className="cart-empty">El carrito esta vacio</p>
+                    )}
+                </div>
+            </div>
+        </header>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: 1,
+        title: "Cien años de soledad",
+        price: 190,
+        urlImage: 'https://prodimage.images-bn.com/pimages/9798890981745_p1_v2_s1200x1200.jpg',
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: "El señor de los anillos(Triologia)",
+        price: 190,
+        urlImage: 'https://es.web.img3.acsta.net/newsv7/21/05/07/15/21/3928581.jpg',
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: "Cuentos de barro",
+        price: 30,
+        urlImage: 'https://m.media-amazon.com/images/I/41dZy8CP52L._SY445_SX342_.jpg',
+        quantity: 1
+    },
+    {
+        id: 4,
+        title: 'Tierra de Infancia',
+        price: 30,
+        urlImage: 'https://leolibri.net/image/cache/catalog/2024/febrero%2026%20ev/Lars-TierradeInfancia-927x1000.jpg',
+        quantity: 1
+    },
+    {
+        id: 5,
+        title: 'Harry Potter Pack',
+        price: 390,
+        urlImage: 'https://i5.walmartimages.com/seo/HARRY-POTTER-Pack-HarryPotter-La-serie-completa-Harry-Potter-Paperback-Boxed-Set-Books-1-7-Paperback-9788418173196_983ae67f-131d-4378-ba10-3ceadef50dde.4d4a65cd092771e4c042cb948e26be06.jpeg',
+        quantity: 1
+    }
+]
